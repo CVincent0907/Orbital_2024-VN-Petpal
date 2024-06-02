d25a0f851392c7869b80c4a037c018972e001b13
CVincent0907 <119739703+CVincent0907@users.noreply.github.com>
2024-06-02 12:15:03 +0800

    Update urls.py
    Added url to backend endpoints, login/ for handling login requests.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 # your_app/urls.py
 from django.urls import path
-from .views import register_step_one, register_step_two, register_step_three
+from .views import register_step_one, register_step_two, register_step_three, login_view
 
 urlpatterns = [
     path('register_step_one/', register_step_one, name='register_step_one'),
     path('register_step_two/', register_step_two, name='register_step_two'),
     path('register_step_three/', register_step_three, name='register_step_three'),
+    path('login/', login_view, name='login' )
 ]
+
