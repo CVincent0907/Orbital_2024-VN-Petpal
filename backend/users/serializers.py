54d347f31daac681dab297fd4d80b52e622988af
Nathan Lye <nathanlye05@gmail.com>
2024-06-13 02:41:16 +0800

    modify user serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,41 +1,53 @@
 # your_app/serializers.py
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 
 UserModel = get_user_model()
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = '__all__'
-
+        # extra_kwargs = {'password': {'write_only': True}}
+    
     def create(self, clean_data):
         user = UserModel.objects.create_user(**clean_data)
         user.save()
         return user
 
 
-class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+# class UserRegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserModel
+#         fields = '__all__'
+
+#     def create(self, clean_data):
+#         user = UserModel.objects.create_user(**clean_data)
+#         user.save()
+#         return user
+
+
+# class UserLoginSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     password = serializers.CharField()
     
-    def check_user(self, clean_data):
-        user = authenticate(username=clean_data['email'], password=clean_data['password'])
-        if not user:
-            raise serializers.ValidationError('user not found')
-        return user
+#     def check_user(self, clean_data):
+#         user = authenticate(username=clean_data['email'], password=clean_data['password'])
+#         if not user:
+#             raise serializers.ValidationError('user not found')
+#         return user
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        exclude = ('password', 'groups', 'user_permissions', 'is_superuser', 'is_staff',)
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserModel
+#         exclude = ('password', 'groups', 'user_permissions', 'is_superuser', 'is_staff',)
 
 
-class EmailIsAvailableSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+# class EmailIsAvailableSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
 
-    def is_available(self, email):
-        user = UserModel.objects.filter(email=email)
-        return not user.exists()
+#     def is_available(self, email):
+#         user = UserModel.objects.filter(email=email)
+#         return not user.exists()
