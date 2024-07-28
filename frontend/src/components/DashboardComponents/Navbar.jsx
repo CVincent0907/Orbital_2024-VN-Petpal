b894b9311f14bf97da0e458bfb9c75f413c97f76
CVincent0907 <119739703+CVincent0907@users.noreply.github.com>
2024-07-28 21:46:20 +0800

    Update Navbar.jsx

--- frontend/src/components/DashboardComponents/Navbar.jsx
+++ frontend/src/components/DashboardComponents/Navbar.jsx
@@ -8,44 +8,44 @@ import ConfirmLogoutModal from './ConfirmLogoutModal';
 
 
 export default function Navbar({ info }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
     const shelterData = useContext(ShelterContext);
-    const navigate = useNavigate();
+    // const [isModalOpen, setIsModalOpen] = useState(false);
+    // const navigate = useNavigate();
 
-    const handleLogoutClick = () => {
-        setIsModalOpen(true);
-    };
+    // const handleLogoutClick = () => {
+    //     setIsModalOpen(true);
+    // };
 
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
+    // const handleCancel = () => {
+    //     setIsModalOpen(false);
+    // };
 
-    const handleConfirm = async () => {
-        axiosInstance.post('/api/auth/logout/')
-        .then((res) => {
-          // Assuming logout is successful
-          alert(`Logout successful! Response data: ${JSON.stringify(res.data)}`);
-          // Navigate to the appropriate page after logout
-          navigate('/'); // Adjust the route as needed
-        })
-        .catch((error) => {
-          alert("Error: " + error.message);
-          console.error(error);
-        });
-    }
+    // const handleConfirm = async () => {
+    //     axiosInstance.post('/api/auth/logout/')
+    //     .then((res) => {
+    //       // Assuming logout is successful
+    //       alert(`Logout successful! Response data: ${JSON.stringify(res.data)}`);
+    //       // Navigate to the appropriate page after logout
+    //       navigate('/'); // Adjust the route as needed
+    //     })
+    //     .catch((error) => {
+    //       alert("Error: " + error.message);
+    //       console.error(error);
+    //     });
+    // }
 
     return (
         <nav className="navbar">
             <img className="side_icon" onClick={info.icon_onClick} src={info.icon_src} alt={info.icon_alt} />
             <img className="logo" src={petpalSvg} alt='petpal' />
             <h1>{info.title}</h1>
-            <button className="logout-button" onClick={handleLogoutClick}>Logout</button>
+{/*             <button className="logout-button" onClick={handleLogoutClick}>Logout</button> */}
             <img className="profile" src={shelterData.profile_pic ? shelterData.profile_pic : imagePlaceholder} alt='profile' />
-            <ConfirmLogoutModal 
+{/*             <ConfirmLogoutModal 
                 isOpen={isModalOpen}
                 onRequestClose={handleCancel}
                 onConfirm={handleConfirm}
-            />
+            /> */}
         </nav>
     );
 }
